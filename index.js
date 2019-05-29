45d946313d37fa6367781d34a7df523ccc8e6d57
martinmolvaer <42579391+martinmolvaer@users.noreply.github.com>
2019-05-29 09:27:50 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,149 @@
+
+var itemContainers = [].slice.call(document.querySelectorAll('.board-column-content'));
+var columnGrids = [];
+var boardGrid;
+
+var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+
+
+document.getElementById('add').addEventListener('click', function(){
+  
+
+  var value = document.getElementById('item').value;
+
+  if (value === ''){
+    
+  } else {
+    addItemTodo(value);
+    document.getElementById('item').value = '';
+  }
+})
+
+// enter funkjer nå
+var input = document.getElementById('item');
+
+input.onkeyup = e => {
+
+  if(e.keyCode == 13){
+      addItemTodo(input.value);
+      input.value = '';
+  }
+};
+
+
+function addItemTodo(text){
+
+
+  var list = document.getElementById('todo')
+  var wrapper = document.createElement('div')
+  wrapper.classList = 'board-item';
+  var item = document.createElement('div');
+  item.classList = 'board-item-content';
+  item.innerText = text;
+  
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+//Creating the remove
+  var remove = document.createElement('button');
+  remove.classList.add('remove')
+  remove.innerHTML = removeSVG;
+
+
+//making buttons for the new task
+  var complete = document.createElement('button');
+  complete.classList.add('complete')
+  complete.innerHTML = completeSVG;
+
+
+  // add click event for removing ^
+  remove.onclick = _ => item.remove();
+  
+
+  //adding buttons to the div
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+  wrapper.appendChild(item);
+
+  if(list.firstElementChild){
+    list.insertBefore(wrapper, list.children[0]);
+    }else{
+    list.appendChild(wrapper);
+}
+
+  grid[0].add(wrapper);
+  
+}
+
+
+
+var grid = [];
+// Define the column grids so we can drag those
+// items around.
+itemContainers.forEach(function (container) {
+
+  // Instantiate column grid.
+    var tempGrid = new Muuri(container, {
+    items: '.board-item',
+    layoutDuration: 400,
+    layoutEasing: 'ease',
+    dragEnabled: true,
+    dragSort: function () {
+      return columnGrids;
+    },
+    dragSortInterval: 0,
+    dragContainer: document.body,
+    dragReleaseDuration: 400,
+    dragReleaseEasing: 'ease'
+  })
+  .on('dragStart', function (item) {
+    // Let's set fixed widht/height to the dragged item
+    // so that it does not stretch unwillingly when
+    // it's appended to the document body for the
+    // duration of the drag.
+    item.getElement().style.width = item.getWidth() + 'px';
+    item.getElement().style.height = item.getHeight() + 'px';
+  })
+  .on('dragReleaseEnd', function (item) {
+    // Let's remove the fixed width/height from the
+    // dragged item now that it is back in a grid
+    // column and can freely adjust to it's
+    // surroundings.
+    item.getElement().style.width = '';
+    item.getElement().style.height = '';
+    // Just in case, let's refresh the dimensions of all items
+    // in case dragging the item caused some other items to
+    // be different size.
+    columnGrids.forEach(function (grid) {
+      grid.refreshItems();
+    });
+  })
+  .on('layoutStart', function () {
+    // Let's keep the board grid up to date with the
+    // dimensions changes of column grids.
+    boardGrid.refreshItems().layout();
+  });
+
+  // Add the column grid reference to the column grids
+  // array, so we can access it later on.
+  columnGrids.push(tempGrid);
+  grid.push(tempGrid);
+
+});
+
+// Instantiate the board grid so we can drag those
+// columns around.
+boardGrid = new Muuri('.board', {
+  layoutDuration: 400,
+  layoutEasing: 'ease',
+  dragEnabled: true,
+  dragSortInterval: 0,
+  dragStartPredicate: {
+    handle: '.board-column-header'
+  },
+  dragReleaseDuration: 400,
+  dragReleaseEasing: 'ease'
+}); 
