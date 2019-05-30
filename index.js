50c272c41ed270fbf14210e591d623063993dac5
martinmolvaer <42579391+martinmolvaer@users.noreply.github.com>
2019-05-30 13:48:09 +0200

    Add files via upload
    v.03

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//random farge på load
+
 
 var itemContainers = [].slice.call(document.querySelectorAll('.board-column-content'));
 var columnGrids = [];
@@ -5,14 +7,13 @@ var boardGrid;
 
 
 var info = [];
+var input = document.getElementById('item');
+var label = document.getElementById('viktig');
+
 
 document.getElementById('add').addEventListener('click', function(){
   
-
   var value = document.getElementById('item').value;
-  var labelGet = document.getElementById('viktig').value;
-
-  console.log(label);
 
   if (value === ''){
     
@@ -23,9 +24,6 @@ document.getElementById('add').addEventListener('click', function(){
 })
 
 // enter for å legge til task
-var input = document.getElementById('item');
-var label = document.getElementById('viktig');
-
 input.onkeyup = e => {
 
   if(e.keyCode == 13){
@@ -37,11 +35,7 @@ input.onkeyup = e => {
 
 function addItemTodo(text){
 
-/*
-  label.createElement('div');
-  label.classList = 'board-item';
-  label.classList = 'board-item-content'; */  
-
+  //lage div til todo og div til items
   var list = document.getElementById('todo')
   var wrapper = document.createElement('div')
   wrapper.classList = 'board-item';
@@ -49,7 +43,7 @@ function addItemTodo(text){
   item.classList = 'board-item-content';
   item.innerText = text;
   
-
+  //lage div til buttons
   var buttons = document.createElement('div');
   buttons.classList.add('buttons');
 
@@ -59,26 +53,32 @@ function addItemTodo(text){
   var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
   remove.innerHTML = removeSVG;
 
-  // add click event for removing ^
-  remove.onclick = _ => item.remove();
+    // add click event for removing
+  remove.addEventListener('click', function(){
+    
+    item.style.display = 'none';
+    // få grid til å refreshe
+    
+})
+
 
   //make labels
   var label = document.createElement('div');
   label.classList.add('label');
   var greenLabelSVG = '<?xml version="1.0" encoding="UTF-8"?><svg enable-background="new 0 0 511.996 511.996" version="1.1" viewBox="0 0 511.996 511.996" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"> <path d="m256.03 511.96c-65.578 0-131.16-24.962-181.08-74.885-6.378-6.378-6.38-16.72 0-23.099 6.378-6.378 16.72-6.38 23.1 0 87.11 87.11 228.85 87.11 315.96 0 87.11-87.11 87.11-228.85 0-315.96-87.11-87.11-228.85-87.11-315.96 0-56.361 56.361-78.483 139.24-57.733 216.3 2.345 8.711-2.814 17.673-11.525 20.018-8.707 2.345-17.673-2.814-20.018-11.525-23.783-88.306 1.575-183.29 66.173-247.89 99.849-99.849 262.31-99.847 362.16 0 99.847 99.849 99.847 262.31 0 362.16-49.925 49.923-115.5 74.886-181.08 74.886z" fill="#507C5C"/> <circle cx="256.04" cy="256.01" r="154.6" fill="#CFF09E"/><path d="m256.03 426.92c-94.246 0-170.92-76.675-170.92-170.92s76.675-170.92 170.92-170.92 170.92 76.675 170.92 170.92-76.676 170.92-170.92 170.92zm0-309.18c-76.234 0-138.26 62.021-138.26 138.26s62.021 138.26 138.26 138.26 138.26-62.021 138.26-138.26-62.021-138.26-138.26-138.26z" fill="#507C5C"/></svg>';
   var yellowLabelSVG = '<?xml version="1.0" encoding="UTF-8"?><svg class="" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path class="active-path" d="m512 256c0 141.39-114.61 256-256 256s-256-114.61-256-256 114.61-256 256-256 256 114.61 256 256z" fill="#FFE800" data-old_color="#E76E54" data-original="#E76E54"/><path class="" d="m384 256c0 70.691-57.309 128-128 128s-128-57.309-128-128 57.309-128 128-128 128 57.309 128 128z" fill="#ff0" dataoriginal="#DD523C"/></svg>'
-  var redLabelSVG = '<?xml version="1.0" encoding="UTF-8"?><svg width="512pt" height="512pt" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m512 256c0 141.39-114.61 256-256 256s-256-114.61-256-256 114.61-256 256-256 256 114.61 256 256z" fill="#e76e54"/><path d="m384 256c0 70.691-57.309 128-128 128s-128-57.309-128-128 57.309-128 128-128 128 57.309 128 128z" fill="#dd523c"/></svg>';
+  var redLabelSVG = '<?xml version="1.0" encoding="UTF-8"?><svg width="17px" height="17px" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="m512 256c0 141.39-114.61 256-256 256s-256-114.61-256-256 114.61-256 256-256 256 114.61 256 256z" fill="#e76e54"/><path d="m384 256c0 70.691-57.309 128-128 128s-128-57.309-128-128 57.309-128 128-128 128 57.309 128 128z" fill="#dd523c"/></svg>';
   
   if(document.getElementById('viktig').value === 'green'){
     label.innerHTML = greenLabelSVG;
+
   } else if ((document.getElementById('viktig').value === 'yellow')){
     label.innerHTML = yellowLabelSVG;
+
   } else if(document.getElementById('viktig').value === 'red') {
     label.innerHTML = redLabelSVG;
   }
 
-  
-  
 
   //adding buttons to the div
   buttons.appendChild(label);
@@ -92,9 +92,9 @@ function addItemTodo(text){
     list.appendChild(wrapper);
 }
 
-
   grid.push(item.textContent);
   grid[0].add(wrapper); 
+
 }
 
 var grid = [];
@@ -115,9 +115,10 @@ itemContainers.forEach(function (container) {
     },
     dragSortInterval: 0,
     dragContainer: document.body,
-    dragReleaseDuration: 400,
+    dragReleaseDuration: 300,
     dragReleaseEasing: 'ease'
   })
+  
   .on('dragStart', function (item) {
     // Let's set fixed widht/height to the dragged item
     // so that it does not stretch unwillingly when
@@ -125,7 +126,8 @@ itemContainers.forEach(function (container) {
     // duration of the drag.
     item.getElement().style.width = item.getWidth() + 'px';
     item.getElement().style.height = item.getHeight() + 'px';
-  })
+    
+    })
   .on('dragReleaseEnd', function (item) {
     // Let's remove the fixed width/height from the
     // dragged item now that it is back in a grid
@@ -138,20 +140,24 @@ itemContainers.forEach(function (container) {
     // be different size.
     columnGrids.forEach(function (grid) {
       grid.refreshItems();
+      grid.refreshSortData();
     });
   })
   .on('layoutStart', function () {
     // Let's keep the board grid up to date with the
     // dimensions changes of column grids.
     boardGrid.refreshItems().layout();
-  });
+    
+  }); 
 
   // Add the column grid reference to the column grids
   // array, so we can access it later on.
   columnGrids.push(tempGrid);
   grid.push(tempGrid);
-
+  
 });
+
+
 
 // Instantiate the board grid so we can drag those
 // columns around.
@@ -166,6 +172,10 @@ boardGrid = new Muuri('.board', {
   dragReleaseDuration: 400,
   dragReleaseEasing: 'ease'
 });
+
+
+
+
 
 // POP-UP JS 
 
@@ -182,5 +192,4 @@ function closeForm() {
 }
 
 function setInvert(){
-  document.body.style.transform = "rotate(-90deg)";
 }   
