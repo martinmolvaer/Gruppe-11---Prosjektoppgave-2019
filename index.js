21a20b9bb5e7ae47785695567cbb5fca0fe58663
martinmolvaer <42579391+martinmolvaer@users.noreply.github.com>
2019-05-31 15:10:30 +0200

    diverse endringer
    litt endringer til knappene til items.
    la til info-knapp som skal åpne info-panelet

--- index.js
+++ index.js
@@ -53,6 +53,16 @@ function addItemTodo(text){
   var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
   remove.innerHTML = removeSVG;
 
+
+
+  var info = document.createElement('div');
+  info.classList.add('info');
+  var infoSVG = "<?xml version='1.0' encoding='UTF-8'?><!DOCTYPE svg  PUBLIC '-//W3C//DTD SVG 1.1//EN'  'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'><svg enable-background='new 0 0 510 510' version='1.1' viewBox='0 0 510 510' xml:space='preserve' xmlns='http://www.w3.org/2000/svg'><path d='m255 0c-140.25 0-255 114.75-255 255s114.75 255 255 255 255-114.75 255-255-114.75-255-255-255zm25.5 382.5h-51v-153h51v153zm0-204h-51v-51h51v51z'/></svg>"
+  info.innerHTML = infoSVG;
+
+
+
+
     // add click event for removing
   remove.addEventListener('click', function(){
     
@@ -60,9 +70,19 @@ function addItemTodo(text){
     // få grid til å refreshe
 })
 
-  item.addEventListener('click', function(){
+  info.addEventListener('click', function(){
 
   modal.style.display = "block";
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
  })
 
@@ -136,9 +156,10 @@ option.setAttribute('id', "Danial")
 
 
   //legge til items til hoved-div
-  buttons.appendChild(label);
-  buttons.appendChild(remove);
-  buttons.appendChild(option);
+  item.appendChild(label);
+  item.appendChild(remove);
+  item.appendChild(info);
+  item.appendChild(option);
   item.appendChild(buttons);
   wrapper.appendChild(item);
 
@@ -157,6 +178,14 @@ var memberSave = [];
 var grid = [];
 
 
+
+
+
+
+
+
+// MURI FRAMWORK
+
 // Define the column grids so we can drag those
 // items around.
 itemContainers.forEach(function (container) {
@@ -170,7 +199,7 @@ itemContainers.forEach(function (container) {
     dragSort: function () {
       return columnGrids;
     },
-    dragStartPredicate:{distance: 0, delay: 100, handle: false},
+    dragStartPredicate:{distance: 0, delay: 70, handle: true},
     dragSortInterval: 0,
     dragContainer: document.body,
     dragReleaseDuration: 300,
@@ -235,6 +264,7 @@ boardGrid = new Muuri('.board', {
 
 
 
+
 // POP-UP JS 
 
 function openForm() {
@@ -261,19 +291,10 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+/*
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
-}
+} */
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
