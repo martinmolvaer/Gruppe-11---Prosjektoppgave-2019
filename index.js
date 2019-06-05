cccb91323a10c9b14b0f7a18e3383ad103063961
martinmolvaer <martin.molvaer@gmail.com>
2019-06-05 16:06:47 +0200

    SPLICING ??

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function getLocalStorage() {
-  localStorage.setItem("data", JSON.stringify(memberSave));
-}
+
 //Instansiating important DOM
 var input = document.getElementById("item");
 var label = document.getElementById("viktig");
@@ -10,10 +8,9 @@ banan.addEventListener("click", function() {
   banan.style.display = "none";
 });
 
-//Adding information to the "memberSave" to be displayed
-var memberSave = localStorage.getItem("data")
-  ? JSON.parse(localStorage.getItem("data"))
-  : [];
+
+// Adding information to the "memberSave" to be displayed
+var memberSave = []
 
 //implementing sortablejs
 //Lage Todo sorterbar og kunne ta i mot elementer
@@ -80,13 +77,16 @@ function addItemTodo(text) {
   var item = document.createElement("div");
   item.classList = "board-item-content";
   item.innerText = text;
+
+  console.log(item.textContent);
   var input = document.getElementById("item");
+
+
 
   //Creating the remove
   var remove = document.createElement("div");
   remove.classList.add("remove");
-  var removeSVG =
-    '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
+  var removeSVG ='<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
   remove.innerHTML = removeSVG;
 
   var info = document.createElement("div");
@@ -98,7 +98,13 @@ function addItemTodo(text) {
   // add click event for removing
   remove.addEventListener("click", function() {
     item.style.display = "none";
-    // få grid til å refreshe array.slice ??
+
+    for (i = 0; i < memberSave.length; i++){
+
+      memberSave.splice(i, 1);
+      console.log(memberSave[i])
+
+    }
   });
 
   var buttons = document.createElement('div');
@@ -189,11 +195,11 @@ function addItemTodo(text) {
   };
   //legge tasks med info fra objekt til i array
   memberSave.push(taskSave);
-  getLocalStorage();
 }
 
-// POP-UP JS
+console.log(memberSave);
 
+// POP-UP JS
 function openForm() {
   document.getElementById("myForm").style.display = "block";
   document.getElementById("help-btn").style.display = "none";
